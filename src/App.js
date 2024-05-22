aaa8eb544ed6406cf71705906545c74f2d00d49b
Gizem Altay <gizemaltay0625@gmail.com>
2024-05-22 12:39:47 +0300

    subPages title fixed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './index.css';
 import { MainPage } from './pages/MainPage';
 import { BaseLayout } from './layouts/BaseLayout';
 import { MainLayout } from './layouts/MainLayout';
-import { InProgressCourses } from './pages/SubPages/InProgress';
-import UpcomingCourses from './pages/SubPages/UpComing';
 import { Skills } from './pages/Skills';
 import { Team } from './pages/Team';
+import { InProgressCourses } from './pages/subPages/InProgress';
+import UpcomingCourses from './pages/subPages/UpComing';
 
 const App = () => {
   return (
